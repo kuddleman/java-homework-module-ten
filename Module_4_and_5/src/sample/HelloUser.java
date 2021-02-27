43e836c579f2068d9a88faba4e3a1237afd81ca5
Kuddleman <edl1155@gmail.com>
2021-02-27 08:18:04 -0800

    set up HelloUser class

---
+++ Module_4_and_5/src/sample/HelloUser.java
@@ -0,0 +1,17 @@
+package sample;
+
+import javafx.application.*;
+import javafx.event.*;
+import javafx.scene.*;  //this does NOT import subpackages
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+import javafx.scene.paint.*;  //paint is a subpackage of scene.  We need to import explicitly
+import javafx.scene.text.*;
+import javafx.stage.*;
+
+public class HelloUser {
+
+    public static void main(String[] args) {
+
+    }
+}
