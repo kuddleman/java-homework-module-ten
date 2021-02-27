22a683c7e9d8ce889fcf0739df8f3622c5158deb
Kuddleman <edl1155@gmail.com>
2021-02-27 09:09:44 -0800

    set up scene, stage

--- Module_4_and_5/src/sample/HelloUser.java
+++ Module_4_and_5/src/sample/HelloUser.java
@@ -2,16 +2,25 @@ package sample;
 
 import javafx.application.*;
 import javafx.event.*;
-import javafx.scene.*;  //this does NOT import subpackages
+import javafx.scene.*;
 import javafx.scene.control.*;
 import javafx.scene.layout.*;
-import javafx.scene.paint.*;  //paint is a subpackage of scene.  We need to import explicitly
+import javafx.scene.paint.*;
 import javafx.scene.text.*;
 import javafx.stage.*;
 
-public class HelloUser {
+public class HelloUser extends Application {
+    public void start(Stage primaryStage) {
+        Pane pane = new FlowPane();
+
+        Scene scene = new Scene(pane, 300, 300, Color.SKYBLUE);
+
+        primaryStage.setTitle("Hello, User!");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
 
     public static void main(String[] args) {
-
+        launch(args);
     }
 }
