6b293187436d5f20e0723abe4f9302d27e9db46c
Kuddleman <edl1155@gmail.com>
2021-02-27 16:23:29 -0800

    nest panes

--- Module_4_and_5/src/sample/IncDecButtons.java
+++ Module_4_and_5/src/sample/IncDecButtons.java
@@ -2,6 +2,7 @@ package sample;
 
 import javafx.application.*;
 import javafx.event.*;
+import javafx.geometry.*;
 import javafx.scene.*;  //this does NOT import subpackages
 import javafx.scene.control.*;
 import javafx.scene.layout.*;
@@ -11,9 +12,27 @@ import javafx.stage.*;
 
 public class IncDecButtons extends Application {
 
+    private Text counterText;
+    private Button incButton, decButton;
+
 
     public void start(Stage primaryStage) {
         VBox mainVBox = new VBox();
+        mainVBox.setStyle("-fx-background-color: cyan");
+        mainVBox.setAlignment(Pos.CENTER);
+
+        counterText = new Text("0");
+        counterText.setFont(Font.font("Helvetica", 28));
+        counterText.setFill(Color.PLUM);
+        mainVBox.getChildren().add(counterText);
+        mainVBox.setSpacing(10);
+
+        incButton = new Button(" + ");
+        decButton = new Button(" - ");
+        HBox buttonBox = new HBox(incButton, decButton);
+        buttonBox.setAlignment(Pos.CENTER);
+        buttonBox.setSpacing(10);
+        mainVBox.getChildren().add(buttonBox);
 
         Scene scene = new Scene(mainVBox, 200, 200);
         primaryStage.setScene(scene);
