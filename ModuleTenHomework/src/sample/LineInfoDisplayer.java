d30d0676ffff4d4faad8dbda133cefed1f8228fb
Kuddleman <edl1155@gmail.com>
2021-04-17 18:53:55 -0700

    initial commit of starter code

---
+++ ModuleTenHomework/src/sample/LineInfoDisplayer.java
@@ -0,0 +1,19 @@
+package sample;
+
+import javafx.scene.shape.Line;
+@FunctionalInterface
+public interface LineInfoDisplayer {
+
+    String getInfo(Line line);
+
+    public static enum InfoType {
+        DISTANCE, MIDPOINT, VERTHORZ;
+    }
+
+    public static LineInfoDisplayer createLineInfoDisplayer(InfoType type) {
+        //  YOUR CODE HERE!
+
+        return null;
+    }
+
+}
